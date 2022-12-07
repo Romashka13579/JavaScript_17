ab9b7f26ec80b90aa07ff2b2a2976b8770fc16f9
Romashka <rroma13579@gmail.com>
2022-12-07 23:51:57 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ async function WebLink(link){
     const page = await browser.newPage();
     await page.goto(link);
 
-    const [el] = await page.$x('//*[@id="global_actions"]/a/img');
-    const src = await el.getProperty('src');
-    const srcTxt = src.jsonValue();
+    const [el] = await page.$x('//*[@id="largeiteminfo_item_name"]');
+    const txt = await el.getProperty('textContent');
+    const Txt = await txt.jsonValue();
 
-    console.log({srcTxt});
+
+    // const [el2] = await page.$x('/html/body/div[1]/div[7]/div[2]/div[1]/div[4]/div/div[2]/div/div[5]/div[4]/div[2]/div[1]/div[1]/div/div[1]/div/span[2]');
+    // const txt2 = await el2.getProperty('textContent');
+    // const Txt2 = await txt2.jsonValue();
+
+    console.log(Txt);
+
+    browser.close();
 }
 
-WebLink('https://store.steampowered.com/');
+WebLink('https://steamcommunity.com/market/listings/730/Recoil%20Case');
