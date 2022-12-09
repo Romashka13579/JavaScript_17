4a399843da7e115e85dbae9b7b5beda9a3946939
Romashka <rroma13579@gmail.com>
2022-12-09 13:32:54 +0100

    fourth commit

--- js/main.js
+++ js/main.js
@@ -6,24 +6,22 @@ async function WebLink(link){
     const page = await browser.newPage();
     await page.goto(link);
 
-    // const [el] = await page.$x('//*[@id="largeiteminfo_item_name"]');
-    // const txt = await el.getProperty('textContent');
-    // const Txt = await txt.jsonValue();
+    const [el] = await page.$x('/html/body/div[1]/div[7]/div[2]/div[1]/div[4]/div/div[2]/div/div[1]/div[1]/img');
+    const txt = await el.getProperty('src');
+    const Txt = await txt.jsonValue();
 
-    const grabTxt = await page.evaluate(() => {
-        const txt = document.querySelector(".hover_item_name");
-        return txt.innerHTML;
-    });
+    // const grabTxt = await page.evaluate(() => {
+    //     const txt = document.querySelector(".hover_item_name");
+    //     return txt.innerHTML;
+    // });
 
-    console.log(grabTxt);
+    // console.log(grabTxt);
 
     // const [el2] = await page.$x('/html/body/div[1]/div[7]/div[2]/div[1]/div[4]/div/div[2]/div/div[5]/div[4]/div[2]/div[1]/div[1]/div/div[1]/div/span[2]');
     // const txt2 = await el2.getProperty('textContent');
     // const Txt2 = await txt2.jsonValue();
 
-    browser.close();
+    browser.close(Txt);
 }
 
 WebLink('https://steamcommunity.com/market/listings/730/Recoil%20Case');
-
-
--- js/main_2.js
+++ js/main_2.js
@@ -1,14 +1,54 @@
-var adderTextlineByLink = document.querySelector('adder-textline-by-link');
-var adderBtnByLink = document.querySelector('adder-btn-by-link');
-var cardsHolder = document.querySelector('cards-holder');
-var holderCards = document.querySelectorAll('holder-card');
+var adderTextlineByLink = document.querySelector('.adder-textline-by-link');
+var adderBtnByLink = document.querySelector('.adder-btn-by-link');
+var cardsHolder = document.querySelector('.cards-holder');
+var holderCards = document.querySelectorAll('.holder-card');
+
+var holderCard = holderCards[0];
 
 adderBtnByLink.addEventListener('click', () => {
     if(adderTextlineByLink.value != ""){
-        import { grabTxt } from './main.js'
-        var holderCardClone = holderCards[0].cloneNode(true);
-        holderCardClone.querySelector('holder-card-name').innerHTML = grabTxt;
+        var holderCardClone = holderCard.cloneNode(true);
+        holderCardClone.querySelector('.holder-card-name').innerHTML = adderTextlineByLink.value;
         cardsHolder.append(holderCardClone);
-        var holderCards = document.querySelectorAll('holder-card');
+        var holderCards = document.querySelectorAll('.holder-card');
     }
 });
+
+var headerBurger = document.querySelector('.header-burger');
+var headerBurgerLines = document.querySelectorAll('.header-burger-line');
+burgerclicked = false;
+
+headerBurger.addEventListener('click', () => {
+    if(burgerclicked == false){
+        headerBurgerLines[0].style.transform = "translateY(14px) rotate(-45deg)";
+        headerBurgerLines[1].style.opacity = 0;
+        headerBurgerLines[2].style.transform = "translateY(-14px) rotate(45deg)";
+        burgerclicked = true;
+    }
+    else{
+        headerBurgerLines[0].style.transform = "translateY(0px) rotate(0deg)";
+        headerBurgerLines[1].style.opacity = 1;
+        headerBurgerLines[2].style.transform = "translateY(0px) rotate(0deg)";
+        burgerclicked = false;
+    }
+})
+
+// var xhr = new XMLHttpRequest();
+
+// xhr.open("GET", "https://cybersoft.neocities.org/index.html", true);
+// xhr.responseType = "document";
+
+// xhr.onload = function(){
+//     if(xhr.readyState == 4 && xhr.status == 200){
+//         var response = xhr.responseXML.querySelector('.home-web-bottom-button');
+
+//         console.log(response.textContent);
+//     }
+// }
+
+// xhr.onerror = function(){
+//     console.log("shit");
+// }
+
+// xhr.send();
+
